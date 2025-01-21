480be6eca56ba594bf46e742611f2580712aba49
Gerald Guerrero <geraldguerrerotech@gmail.com>
2025-01-20 22:10:15 -0500

    Updated code for proper syntax in student_classes.sql

--- student_classes.sql
+++ student_classes.sql
@@ -1,6 +1,6 @@
 SELECT stu.first_name, stu.last_name, cla.class_id, cou.course_name, cla.building, cla.room
 FROM Students stu
-JOIN Enrollments enr ON enr.student_id = stu.student_id
-JOIN Classes cla ON cla.class_id = enr.class_id
-Join Courses cou ON cou.course_id = cla.course_id
+JOIN Enrollments enr ON stu.student_id = enr.student_id
+JOIN Classes cla ON enr.class_id = cla.class_id
+Join Courses cou ON cla.course_id = cou.course_id
 WHERE stu.student_id = 1;
